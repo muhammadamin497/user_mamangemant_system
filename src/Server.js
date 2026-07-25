4e5c78acb0cd91247e954caf7b5e97449dc44112
muhammad amin <muhammadamin137340@gmail.com>
2026-07-25 14:31:02 +0500

    fix the dublecat code

--- src/Server.js
+++ src/Server.js
@@ -2,19 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB=require("./Config/db")
 
-const PORT = process.env.PORT;
-
-const app = express();
-dotenv.config();
-connectDB();
-
-app.get("/", (req, res) => {
-  res.send("Server running");
-});
-
-app.listen( PORT,() => {
-  console.log('http://localhost:5000');
-
 const userRoutes = require("./Routes/user_routes");
 const authroutes=require("./Routes/authroutes")
 const adminRoutes=require("./Routes/admainroutes")
