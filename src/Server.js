df6cce8bd5e0fbbca0e49785ead1cb6f56155395
muhammad amin <muhammadamin137340@gmail.com>
2026-07-25 13:14:38 +0500

    import handler middelware '

--- src/Server.js
+++ src/Server.js
@@ -4,6 +4,7 @@ const connectDB=require("./Config/db")
 const userRoutes = require("./Routes/user_routes");
 const authroutes=require("./Routes/authroutes")
 const adminRoutes=require("./Routes/admainroutes")
+const errorHandler = require("./Middleware/handlererror");
 
 
 const app = express();
@@ -15,6 +16,10 @@ connectDB();
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authroutes);
 app.use("/api/admin", adminRoutes);
+
+
+//  error handler 
+app.use(errorHandler);
 
 app.listen( 3000,() => {
   console.log('http://localhost:3000');
