d709976c867a68cbf89fe6fdf2b7d8770a20c31d
muhammad amin <muhammadamin137340@gmail.com>
2026-07-25 10:54:09 +0500

    fix : the srever carshing error

--- src/Server.js
+++ src/Server.js
@@ -1,20 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB=require("./Config/db")
-
-const PORT = process.env.PORT;
-
-const app = express();
-dotenv.config();
-connectDB();
-
-app.get("/", (req, res) => {
-  res.send("Server running");
-});
-
-app.listen( PORT,() => {
-  console.log('http://localhost:5000');
-
 const userRoutes = require("./Routes/user_routes");
 const authroutes=require("./Routes/authroutes")
 const adminRoutes=require("./Routes/admainroutes")
@@ -32,5 +18,4 @@ app.use("/api/admin", adminRoutes);
 
 app.listen( 3000,() => {
   console.log('http://localhost:3000');
-
 });
